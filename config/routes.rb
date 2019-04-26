c8327ca00731203525fd79a753458d51da279aa1
Jaldhi-Joshi <jaldhi.bjoshi@gmail.com>
2019-04-26 01:37:20 -0700

    updated 26 april 6 pm

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :items
+  root 'items#index'
+
+  get 'items/:id', to: 'items#show'
+
+  get 'items/:id/edit', to: 'items#edit'
 end
